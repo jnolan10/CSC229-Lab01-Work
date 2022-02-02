6992baeadff142de839f42f2ebd4c49d809ea9a4
jonat <jonat@137.125.128.251>
2022-02-02 12:57:16 -0500

    First Lab Work CSC229 finished product

--- src/main/java/com/mycompany/csc229_lab01_work/LabWork.java
+++ src/main/java/com/mycompany/csc229_lab01_work/LabWork.java
@@ -2,4 +2,55 @@ package com.mycompany.csc229_lab01_work;
 
 public class LabWork {
     
+    public int sumPrime(int n){
+       int sum = 0;
+       for(int i = 0; i < n; i++){
+           if(isPrime(i) == true)
+               sum += i;
+       }
+       
+       return sum;
+    }
+    
+    public int sumPrimewith3(int n){
+        int sum = 0; 
+        for(int i = 0; i < n; i++){
+            if((isPrime(i) && hasDigit3(i)) == true )
+                sum+= i;
+        }
+        
+        return sum;
+    }
+    
+    public int sumPrimewith3and5(int n){
+        int sum = 0; 
+        for(int i = 0; i < n; i++){
+            if((isPrime(i) && hasDigit3(i) && hasDigit5(i)) == true )
+                sum+= i;
+        }
+        
+        return sum;
+    }
+    
+    public static boolean isPrime(int x){
+        for(int i = 2; i < x ; i++)
+        {
+            if((x % i) == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    public static boolean hasDigit3(int n){
+        String str = ""+n;
+        
+        return str.contains("3");
+    }
+    
+    public static boolean hasDigit5(int n){
+        String str = ""+n;
+        
+        return str.contains("5");
+    }
+    
 }
